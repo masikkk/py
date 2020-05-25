7e1b80af3eb7a9f0f3a5dbc6a91be46c16a1d213
si.ma <si.ma@nio.com>
2020-05-25 19:15:59 +0800

    不更新update_time

--- sql_generator.py
+++ sql_generator.py
@@ -1,14 +1,17 @@
 def update_user_sql_not_in_transaction(start, *end):
     if len(end) > 0:
-        return "update user u set u.identity = 1 where u.id not in (select distinct user_id from user_role_transaction) and u.id >= %d and u.id < %d;\n" % (start, end[0])
+        return "update user u set u.identity = 1, u.update_time = u.update_time where u.id not in (select distinct user_id from user_role_transaction) and u.id >= %d and u.id < %d;\n" % (
+            start, end[0])
     else:
-        return "update user u set u.identity = 1 where u.id not in (select distinct user_id from user_role_transaction) and u.id >= %d;\n" % start
+        return "update user u set u.identity = 1, u.update_time = u.update_time where u.id not in (select distinct user_id from user_role_transaction) and u.id >= %d;\n" % start
+
 
 def update_user_sql_in_transaction(start, *end):
     if len(end) > 0:
-        return "UPDATE user u JOIN user_role_transaction urt ON urt.user_id = u.id SET u.identity = urt.user_identity WHERE urt.user_id = u.id AND NOT exists(SELECT 1 FROM user_role_transaction urt2 WHERE urt2.user_id = urt.user_id AND urt2.id > urt.id) and u.id >= %d and u.id < %d;\n" % (start, end[0])
+        return "UPDATE user u JOIN user_role_transaction urt ON urt.user_id = u.id SET u.identity = urt.user_identity, u.update_time = u.update_time WHERE urt.user_id = u.id AND NOT exists(SELECT 1 FROM user_role_transaction urt2 WHERE urt2.user_id = urt.user_id AND urt2.id > urt.id) and u.id >= %d and u.id < %d;\n" % (
+            start, end[0])
     else:
-        return "UPDATE user u JOIN user_role_transaction urt ON urt.user_id = u.id SET u.identity = urt.user_identity WHERE urt.user_id = u.id AND NOT exists(SELECT 1 FROM user_role_transaction urt2 WHERE urt2.user_id = urt.user_id AND urt2.id > urt.id) and u.id >= %d;\n" % start
+        return "UPDATE user u JOIN user_role_transaction urt ON urt.user_id = u.id SET u.identity = urt.user_identity, u.update_time = u.update_time WHERE urt.user_id = u.id AND NOT exists(SELECT 1 FROM user_role_transaction urt2 WHERE urt2.user_id = urt.user_id AND urt2.id > urt.id) and u.id >= %d;\n" % start
 
 
 # 不在 user_role_transaction 表中的 user
@@ -21,6 +24,7 @@ def generate_update_user_sql_not_in_transaction(linse):
             pre = i
         f.write(update_user_sql_not_in_transaction(pre))
 
+
 # 在 user_role_transaction 表中的 user
 def generate_update_user_sql_in_transaction(lines):
     with open("update_users.sql", 'a') as f:
@@ -31,7 +35,8 @@ def generate_update_user_sql_in_transaction(lines):
             pre = i
         f.write(update_user_sql_in_transaction(pre))
 
+
 if __name__ == "__main__":
-    lineCount = 646578 # user表行数
+    lineCount = 646578  # user表行数
     generate_update_user_sql_not_in_transaction(lineCount)
     generate_update_user_sql_in_transaction(lineCount)
