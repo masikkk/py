bb75f38ebffe31cad52c58c66eee22439bcabeec
masikkk <masi.1989@163.com>
2020-11-28 20:28:47 +0800

    命令行参数

--- sql_gen/count_multi_table.py
+++ sql_gen/count_multi_table.py
@@ -1,4 +1,7 @@
 # coding=utf-8
+
+import sys
+
 # 每个分表的count语句
 def count_table(table_name, column_name, column_value):
     return "select count(*) cnt from %s where %s='%s'" % (table_name, column_name, column_value)
@@ -17,4 +20,11 @@ def sum_count(table_name_prefix, column_name, column_value):
 
 
 if __name__ == "__main__":
-    print sum_count("t_user_record_", "groupid", "collection_test_3")
+    print '参数：', str(sys.argv)
+    if len(sys.argv) > 1:
+        sql = sum_count("t_pic_record_", "groupid", sys.argv[1])
+    else:
+        sql = sum_count("t_pic_record_", "groupid", "collection_test_3")
+    print sql
+    with open("count_multi_table.sql", 'w') as f:
+        f.write(sql)
